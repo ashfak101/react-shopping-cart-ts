96631a6cf0fb24d31c3b84d456c7ba8ff1bd2f5c
Md Ashfakul Karim Rahi <ashfakulkarim2@gmail.com>
2022-04-12 12:55:24 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box } from '@mui/system';
+import React, { useEffect } from 'react';
+import Drawer from '@mui/material/Drawer';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Badge from '@mui/material/Badge';
+import { useQuery } from 'react-query';
+// Types
 
-function App() {
+export type CartItemType ={
+              id:number;
+              category:string;
+              description:string;
+              image:string;
+              price:number;
+              title:string;
+              amount:number }
+const getProducts = async (): Promise<CartItemType[]> =>await(await fetch('https://fakestoreapi.com/products')).json();
+
+const  App=()=> {
+      const {data,isLoading,error}= useQuery<CartItemType[]>('products',
+           getProducts
+      )
+      console.log('Data',data,isLoading,'error',error) 
+      
+      
+      const getTotalItems=()=>null;
+      const handleAddToCart=()=>null;
+      const handleRemoveFromCart=()=>null;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Box>Start</Box>
   );
 }
 
